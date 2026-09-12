02b8fc5eefd50147bc457b691e4e0db34a084349
Desplanches-Eduardo <desplancheseduardo@gmail.com>
2026-09-11 22:34:44 -0300

    Feat: Coloca o Lombok na classe para abstracao

--- src/main/java/desplanches/eduardo/cadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/desplanches/eduardo/cadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package desplanches.eduardo.cadastroDeNinjas.Missoes;
 
 import desplanches.eduardo.cadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
 
     @id
--- src/main/java/desplanches/eduardo/cadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/desplanches/eduardo/cadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,10 +1,18 @@
 package desplanches.eduardo.cadastroDeNinjas.Ninjas;
 import desplanches.eduardo.cadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 //Entidade
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class NinjaModel {
 
     @Id
@@ -19,41 +27,11 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id")// Foreign Key
     private MissoesModel missoes;
 
-    public NinjaModel() {
 
-    }
 
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }
+
+
+
+
+
